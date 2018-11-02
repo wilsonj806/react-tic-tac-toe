60f5d480289684ae4cfcb9dbe23dfab55a737b77
Wilson <wilsonj806@gmail.com>
2018-11-02 18:49:49 -0400

    Updates

---
+++ precache-manifest.fc90b2db29ff3a9f8878a705abce91d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "206a97a05bfbf97c26fe",
+    "url": "/react-tic-tac-toe/static/css/main.fe61c321.chunk.css"
+  },
+  {
+    "revision": "206a97a05bfbf97c26fe",
+    "url": "/react-tic-tac-toe/static/js/main.206a97a0.chunk.js"
+  },
+  {
+    "revision": "8391c07e1621390095e6",
+    "url": "/react-tic-tac-toe/static/js/1.8391c07e.chunk.js"
+  },
+  {
+    "revision": "87d698cc4d3f04a284f2",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.87d698cc.js"
+  },
+  {
+    "revision": "f04908c8a3ad7ecb9b6868470d32ca9b",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
